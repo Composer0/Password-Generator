8d01faa50c4e4d1f6080fae8c899f4b16147f8f6
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-02-23 22:06:46 -0500

    Added files
    Trinity added for use and download once I reach work in the morning to continue.

---
+++ script.js
@@ -0,0 +1,7 @@
+const settings = document.querySelector('settings')
+const setting = document.querySelector('setting')
+const upperCase = document.getElementById('uppercase')
+const lowerCase = document.getElementById('lowercase')
+const numbers = document.getElementById('numbers')
+const symbos = document.getElementById('symbols')
+
