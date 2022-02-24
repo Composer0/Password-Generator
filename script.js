544b769ffb54b4891fe0e66d803a110c2036f5de
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-02-24 14:55:05 -0500

    Uploading Updated File
    Not complete at this time.

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-const settings = document.querySelector('settings')
-const setting = document.querySelector('setting')
-const upperCase = document.getElementById('uppercase')
-const lowerCase = document.getElementById('lowercase')
-const numbers = document.getElementById('numbers')
-const symbos = document.getElementById('symbols')
+// const settings = document.querySelector('settings')
+// const setting = document.querySelector('setting')
+// const upperCase = document.getElementById('uppercase')
+// const lowerCase = document.getElementById('lowercase')
+// const numbers = document.getElementById('numbers')
+// const symbols = document.getElementById('symbols')
 
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    Symbol: getRandomSymbol
+}
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+    // fromCharCode - method returns a string created from the specified sequence of UTF-16 code unites.
+    // refering to character sets from w3schools will be crucial.
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>,.'
+    return symbols[Math.floor(Math.random()*symbols.length)]
+}
+
+console.log(getRandomSymbol())
