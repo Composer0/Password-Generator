4b37c0a6e440370bbc36a17c5e2492964ad9d1d0
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-02-24 20:06:14 -0500

    Final Files
    Trinity of finished project added.

--- script.js
+++ script.js
@@ -1,15 +1,66 @@
-// const settings = document.querySelector('settings')
-// const setting = document.querySelector('setting')
-// const upperCase = document.getElementById('uppercase')
-// const lowerCase = document.getElementById('lowercase')
-// const numbers = document.getElementById('numbers')
-// const symbols = document.getElementById('symbols')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
 
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
-    number: getRandomNumber,
-    Symbol: getRandomSymbol
+    numbers: getRandomNumber,
+    symbols: getRandomSymbol
+}
+
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea')
+    const password = resultEl.innerText
+
+    if(!password) { return }
+
+    textarea.value = password
+    document.body.appendChild(textarea)
+    textarea.select()
+    document.execCommand('copy')
+    textarea.remove()
+    alert('Password copied to clipboard!')
+    // copy is like click. Its an order that is already known.
+})
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value
+    // value are open to interpret booleans, strings, arrays, numbers.
+    const hasLower = lowercaseEl.checked
+    const hasUpper = uppercaseEl.checked
+    const hasNumber = numbersEl.checked
+    const hasSymbol = symbolsEl.checked
+    
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
+})
+
+function generatePassword(lower, upper, numbers, symbols, length) {
+    let generatedPassword = ''
+    const typesCount = lower + upper + numbers + symbols
+    const typesArray = [{lower}, {upper}, {numbers}, {symbols}].filter(item => Object.values(item)[0])
+    // filter(item => Object.values(item)[0] gets rid of information that presents a false value.)
+    
+    if(typesCount === 0) {
+        return ''
+    }
+
+    for(let i = 0; i < length; i += typesCount) {
+        // i += typesCounts. Increments until it equals the typesCount which is set to 20 currently.
+        typesArray.forEach(type => {
+            const funcName = Object.keys(type)[0]
+            generatedPassword += randomFunc[funcName]()
+        })
+
+    }
+    const finalPassword = generatedPassword.slice(0,length)
+
+    return finalPassword
 }
 
 function getRandomLower() {
@@ -30,5 +81,3 @@ function getRandomSymbol() {
     const symbols = '!@#$%^&*(){}[]=<>,.'
     return symbols[Math.floor(Math.random()*symbols.length)]
 }
-
-console.log(getRandomSymbol())
